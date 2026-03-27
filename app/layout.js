42f8b19706a7c85eb9a85d48f40b420360e65b62
Parker Brandt <dante.smith@quickchoice.net>
2026-03-27 14:06:27 -0700

    google tag manager

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ import { buildOrganizationSchema } from '@/lib/structured-data';
 import { getSiteUrl, SITE_NAME } from '@/lib/site';
 
 const GOOGLE_TAG_ID = 'AW-18041825199';
+const GOOGLE_TAG_MANAGER_ID = 'GTM-5K466CPN';
 
 export const metadata = {
   metadataBase: new URL(getSiteUrl()),
@@ -33,7 +34,28 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en" suppressHydrationWarning>
+      {shouldIncludeGoogleTag ? (
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');
+          `}
+        </Script>
+      ) : null}
       <body suppressHydrationWarning style={{ '--app-bg-image': appBackgroundImage }}>
+        {shouldIncludeGoogleTag ? (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        ) : null}
         {shouldIncludeGoogleTag ? (
           <>
             <Script
